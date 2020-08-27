3c8e9d80e24460e5af9cb392a7c72f6b176bb834
Racoveanu Stefan <Contact@cshark.dev>
2020-08-27 16:50:28 +0300

    Added folder Ziua1

---
+++ Ziua1/cmake-build-debug/CMakeFiles/Ziua1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ziua1.dir/main.cpp.o"
+  "Ziua1"
+  "Ziua1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ziua1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
