5adaa5275f320e84978e2c369a42762896c311dd
Racoveanu Stefan <contact@cshark.dev>
2020-09-04 10:05:49 +0300

    Added Ziua3

---
+++ Ziua6/cmake-build-debug/CMakeFiles/Ziua6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ziua6.dir/main.cpp.o"
+  "Ziua6"
+  "Ziua6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ziua6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
